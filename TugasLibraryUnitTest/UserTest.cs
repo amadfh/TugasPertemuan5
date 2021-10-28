d6ee88f6de22ed704c98186e864718b8c5677693
ahmadfh <91525165+amadfh@users.noreply.github.com>
2021-10-28 09:35:48 +0700

    upload tugas

---
+++ TugasLibraryUnitTest/UserTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    class UserTest
+    {
+        
+        private User user;
+
+        [SetUp]
+        public void Init()
+        {
+            user = new User();
+        }
+
+        [Test]
+        public void IsValidUser()
+        {
+            //Assert.IsTrue(user.IsValidUser("admin", "admin"));
+            Assert.IsTrue(user.IsValidUser("admin", "admin"));
+            Assert.IsFalse(user.IsValidUser("admin", "paijo"));
+        }
+    }
+}

95c3f1c1012c07f908d7f9209df9f004e1e8851c
ahmadfh <91525165+amadfh@users.noreply.github.com>
2021-10-28 09:35:16 +0700

    upload tugas

---
+++ TugasLibraryUnitTest/DayTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    class DayTest
+    {
+        private Day day;
+        [SetUp]
+        public void init()
+        {
+            day = new Day();
+        }
+        [Test]
+        public void NameOfDay()
+        {
+
+            Assert.AreEqual("Senin", day.NameOfDay(1));
+            Assert.AreEqual("Selasa", day.NameOfDay(2));
+            Assert.AreEqual("Rabu", day.NameOfDay(3));
+            Assert.AreEqual("Kamis", day.NameOfDay(4));
+            Assert.AreEqual("Jum'at", day.NameOfDay(5));
+            Assert.AreEqual("Sabtu", day.NameOfDay(6));
+            Assert.AreEqual("Minggu", day.NameOfDay(7));
+        }
+    }
+}
